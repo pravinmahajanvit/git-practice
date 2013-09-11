8050bf9b47798b095c1c6565c04c0b3895aeb383
Pravin Mahajan <pravinm@vitpdc-06.(none)>
2013-09-11 17:30:25 +0530

    changes in helloworld.java

--- Maven-Practice-Projects/my-app-manual/src/main/HelloWorld.java
+++ Maven-Practice-Projects/my-app-manual/src/main/HelloWorld.java
@@ -1,6 +1,5 @@
-class HelloWorld{
-public static void main(String args[])
-{
-Syste.out.println("Hello World");
-}
+class HelloWorld {	
+public static void main(String args[]) {
+		System.out.println("Hello World");
+	}
 }
